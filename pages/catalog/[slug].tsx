3a37c8339284c7e93240598f3e54915f3a4a2d61
Nanda Surya <nandasurya@Nandas-MacBook-Air.local>
2022-12-21 17:50:57 +0800

    tesss

--- pages/catalog/[slug].tsx
+++ pages/catalog/[slug].tsx
@@ -87,7 +87,6 @@ export default function detail() {
                   Pesan Sekarang
                 </a>
               </button>
-              <div></div>
             </div>
           </div>
         </div>
