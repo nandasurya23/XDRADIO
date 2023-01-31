e782721f15a289ef66434f247d0decb6adfd885e
Nanda Surya <nandasurya@Nandas-MacBook-Air.local>
2023-01-31 10:55:48 +0800

    first commit

--- pages/catalog.tsx
+++
@@ -1,9 +0,0 @@
-import All_Catalog from "./catalog/All_Catalog";
-
-export default function catalog() {
-  return (
-    <>
-      <All_Catalog />
-    </>
-  );
-}
--- pages/contact.tsx
+++
@@ -1,9 +0,0 @@
-import Contact_Us from "./contact/Contact_Us";
-
-export default function contact() {
-  return (
-    <>
-      <Contact_Us />
-    </>
-  );
-}
