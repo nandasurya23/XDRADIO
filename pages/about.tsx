a46dfa0afd4355dd95fa400eba93da023b5d6c3a
Nanda Surya <nandasurya@Nandas-MacBook-Air.local>
2023-02-06 22:49:51 +0800

    update fix sangat

--- pages/about.tsx
+++ pages/about.tsx
@@ -13,100 +13,103 @@ const about = () => {
     >
       <div className="container px-3 mx-auto">
         <div className="flex flex-wrap justify-between items-center">
-          {/* Komposisi Music */}
-          <div className="px-5 py-5 bg-prime-3  w-full rounded-2xl mt-5 md:self-start md:w-auto md:mt-0">
-            <h1 className="text-xl xd-prime-b text-prime-2 font-semibold md:text-3xl">
-              Kompesisi <span className="text-white">Musik</span>
-            </h1>
-            <div className="xd-prime-r mt-3">
-              <div className="flex flex-col ">
-                <p className="text-lg text-prime-2">Lagu Mancanegara</p>
-                <p>
-                  45 <span>%</span>
-                </p>
+          <div className="flex flex-col justify-between w-full items-center px-5 py-5 rounded-2xl bg-prime-3 lg:flex-row lg:self-start lg:w-auto lg:mt-0">
+            {/* PROFILE RADIO */}
+            <div className="px-5 py-5 bg-prime-3  w-full rounded-2xl mt-5 md:self-start md:w-auto md:mt-0">
+              <h1 className="text-xl xd-prime-b text-prime-2 font-semibold md:text-3xl">
+                Profile <span className="text-white">Radio</span>
+              </h1>
+              <div className="xd-prime-r mt-3">
+                <div className="flex flex-col ">
+                  <p className="text-lg text-prime-2">Badan Hukum</p>
+                  <p>PT . Radio Suara Yudha</p>
+                </div>
+                <div className="flex flex-col mt-3">
+                  <p className="text-lg text-prime-2">Station Radio</p>
+                  <p>XD Radio</p>
+                </div>
+                <div className="flex flex-col mt-3">
+                  <p className="text-lg text-prime-2">Station Call</p>
+                  <p>Your Extra Ordinary Music Station</p>
+                </div>
+                <div className="flex flex-col mt-3">
+                  <p className="text-lg text-prime-2">Frequnsi</p>
+                  <p>90.2 Fm</p>
+                </div>
+                <div className="flex flex-col mt-3">
+                  <p className="text-lg text-prime-2">Alamat</p>
+                  <p>Jln. Tukad Badung No 135 Denpasar</p>
+                </div>
+                <div className="flex flex-col mt-3">
+                  <p className="text-lg text-prime-2">Telp</p>
+                  <p>+62 831-1422-6818</p>
+                </div>
+                <div className="flex flex-col mt-3">
+                  <p className="text-lg text-prime-2">Email</p>
+                  <p>xdradio@primakara.ac.id</p>
+                </div>
+                <div className="flex flex-col mt-3">
+                  <p className="text-lg text-prime-2">Website</p>
+                  <p>xdradiofm.com</p>
+                </div>
+                <div className="flex flex-col mt-3">
+                  <p className="text-lg text-prime-2">Tema Radio</p>
+                  <p>Digital , Creative , Lifestyle</p>
+                </div>
+                <div className="flex flex-col mt-3">
+                  <p className="text-lg text-prime-2">Area Jangkauan</p>
+                  <p>
+                    Denpasar, Badung, Gianyar,
+                    <br />
+                    Bangli, Klungkung, Karangasem,
+                    <br />
+                    Tabanan
+                  </p>
+                </div>
               </div>
-              <div className="flex flex-col mt-3">
-                <p className="text-lg text-prime-2">Lagu Indonesia</p>
-                <p>
-                  40 <span>%</span>
-                </p>
-              </div>
-              <div className="flex flex-col mt-3">
-                <p className="text-lg text-prime-2">Lagu Korea</p>
-                <p>
-                  15 <span>%</span>
-                </p>
-              </div>
-              <div className="flex flex-col mt-3">
-                <p className="text-lg text-prime-2">On Air Aura</p>
-                <p>Cherfull & Fun</p>
-              </div>
-              <div className="flex flex-col mt-3">
-                <p className="text-lg text-prime-2">Operation Time</p>
-                <p>5.30 am - 00.00 pm</p>
-              </div>
-              <div className="flex flex-col mt-3">
-                <p className="text-lg text-prime-2">Prime Time</p>
-                <p>15.00 pm - 20.00 pm</p>
+            </div>
+            {/* Komposisi Music */}
+            <div className="px-5 py-5 bg-prime-3  w-full rounded-2xl mt-5 md:self-start md:w-auto md:mt-0">
+              <h1 className="text-xl xd-prime-b text-prime-2 font-semibold md:text-3xl">
+                Kompesisi <span className="text-white">Musik</span>
+              </h1>
+              <div className="xd-prime-r mt-3">
+                <div className="flex flex-col ">
+                  <p className="text-lg text-prime-2">Lagu Mancanegara</p>
+                  <p>
+                    45 <span>%</span>
+                  </p>
+                </div>
+                <div className="flex flex-col mt-3">
+                  <p className="text-lg text-prime-2">Lagu Indonesia</p>
+                  <p>
+                    40 <span>%</span>
+                  </p>
+                </div>
+                <div className="flex flex-col mt-3">
+                  <p className="text-lg text-prime-2">Lagu Korea</p>
+                  <p>
+                    15 <span>%</span>
+                  </p>
+                </div>
+                <div className="flex flex-col mt-3">
+                  <p className="text-lg text-prime-2">On Air Aura</p>
+                  <p>Cherfull & Fun</p>
+                </div>
+                <div className="flex flex-col mt-3">
+                  <p className="text-lg text-prime-2">Operation Time</p>
+                  <p>5.30 am - 00.00 pm</p>
+                </div>
+                <div className="flex flex-col mt-3">
+                  <p className="text-lg text-prime-2">Prime Time</p>
+                  <p>15.00 pm - 20.00 pm</p>
+                </div>
               </div>
             </div>
           </div>
-          {/* PROFILE RADIO */}
-          <div className="px-5 py-5 bg-prime-3  w-full rounded-2xl mt-5 md:self-start md:w-auto md:mt-0">
-            <h1 className="text-xl xd-prime-b text-prime-2 font-semibold md:text-3xl">
-              Profile <span className="text-white">Radio</span>
-            </h1>
-            <div className="xd-prime-r mt-3">
-              <div className="flex flex-col ">
-                <p className="text-lg text-prime-2">Badan Hukum</p>
-                <p>PT . Radio Suara Yudha</p>
-              </div>
-              <div className="flex flex-col mt-3">
-                <p className="text-lg text-prime-2">Station Radio</p>
-                <p>XD Radio</p>
-              </div>
-              <div className="flex flex-col mt-3">
-                <p className="text-lg text-prime-2">Station Call</p>
-                <p>Your Extra Ordinary Music Station</p>
-              </div>
-              <div className="flex flex-col mt-3">
-                <p className="text-lg text-prime-2">Frequnsi</p>
-                <p>90.2 Fm</p>
-              </div>
-              <div className="flex flex-col mt-3">
-                <p className="text-lg text-prime-2">Alamat</p>
-                <p>Jln. Tukad Badung No 135 Denpasar</p>
-              </div>
-              <div className="flex flex-col mt-3">
-                <p className="text-lg text-prime-2">Telp</p>
-                <p>+62 831-1422-6818</p>
-              </div>
-              <div className="flex flex-col mt-3">
-                <p className="text-lg text-prime-2">Email</p>
-                <p>xdradio@primakara.ac.id</p>
-              </div>
-              <div className="flex flex-col mt-3">
-                <p className="text-lg text-prime-2">Website</p>
-                <p>xdradiofm.com</p>
-              </div>
-              <div className="flex flex-col mt-3">
-                <p className="text-lg text-prime-2">Tema Radio</p>
-                <p>Digital , Creative , Lifestyle</p>
-              </div>
-              <div className="flex flex-col mt-3">
-                <p className="text-lg text-prime-2">Area Jangkauan</p>
-                <p>
-                  Denpasar, Badung, Gianyar,
-                  <br />
-                  Bangli, Klungkung, Karangasem,
-                  <br />
-                  Tabanan
-                </p>
-              </div>
-            </div>
-          </div>
+
           {/* RATE CARD */}
-          <div className="px-5 py-5 bg-prime-3  w-full rounded-2xl mt-5 md:self-start md:w-auto md:mt-0">
+          <div className="px-5 py-5 bg-prime-3  w-full rounded-2xl mt-10  md:self-start md:w-auto lg:mt-0">
             <h1 className="text-xl xd-prime-b text-prime-2 font-semibold md:text-3xl">
               Rate <span className="text-white">Card</span>
             </h1>
