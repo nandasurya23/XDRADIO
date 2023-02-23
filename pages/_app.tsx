df173e04682631c745db6ff61f1cf63adc167832
Nanda Surya <nandasurya@Nandas-MacBook-Air.local>
2023-02-23 23:41:08 +0800

    update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,6 +8,10 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
+        <meta
+          name="google-site-verification"
+          content="3kV_SGei1mMW-Y6nMrAtsZUI1Mzwg18UVzsOMfg1Q-E"
+        />
         <link
           rel="shortcut icon"
           href="https://res.cloudinary.com/df82hir2r/image/upload/v1675135466/xdradio/18_utaonl.png"
