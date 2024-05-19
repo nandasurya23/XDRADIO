d50aa3a85037abcb9a046809efbbe5bb48be8b16
nandasurya23 <nandasurya678@gmail.com>
2024-05-19 21:53:54 +0800

    update

--- pages/api/data/articel.ts
+++ pages/api/data/articel.ts
@@ -2,25 +2,25 @@ export const articel = [
   {
     id: 1,
     image:
-      "https://res.cloudinary.com/df82hir2r/image/upload/v1684289104/xdradio/article/timnas-indonesia-u-22-vs-thailand-8_169_dhb2he.jpg",
-    title: "Klasemen Akhir SEA Games 2023: Timnas Juara, Indonesia Peringkat 3",
-    slug: "Timnas Indonesia U-22 menyumbang keping medali emas yang membuat kontingen Merah Putih menempati peringkat ketiga pada klasemen SEA Games 2023.",
-    link: "https://www.cnnindonesia.com/olahraga/20230517050301-178-950560/klasemen-akhir-sea-games-2023-timnas-juara-indonesia-peringkat-3",
+      "https://res.cloudinary.com/df82hir2r/image/upload/v1716126132/xdradio/article/articel1_w5itwq.png",
+    title: "Starlink Resmi Diluncurkan di RI Hari ini",
+    slug: "Layanan internet satelit Starlink bakal resmi diluncurkan di Indonesia dari Bali, Minggu (19/5), oleh bos SpaceX, miliarder AS Elon Musk.",
+    link: "https://www.cnnindonesia.com/teknologi/20240519054012-213-1099417/starlink-resmi-diluncurkan-di-ri-hari-ini",
   },
   {
     id: 2,
     image:
-      "https://res.cloudinary.com/df82hir2r/image/upload/v1684289104/xdradio/article/hasil-valorant-dan-crossfire-di-sea-games-2023_169_hkqmkq.jpg",
-    title: "Esports SEA Games 2023: Indonesia Juara Umum, 3 Emas, 2 Perak",
-    slug: "Jakarta - Akhirnya seluruh laga di cabang olahraga (cabor) esports SEA Games 2023 Kamboja telah berakhir. Indonesia jadi juara umum di cabor esports, dengan membawa pulang lima medali terdiri dari 3 emas dan 2 perak.",
-    link: "https://inet.detik.com/games-news/d-6722451/esports-sea-games-2023-indonesia-juara-umum-3-emas-2-perak",
+      "https://res.cloudinary.com/df82hir2r/image/upload/v1716126516/xdradio/article/articel2_xyupci.jpg",
+    title: "Review Film: How To Make Millions Before Grandma Dies ",
+    slug: "Thailand lagi-lagi melahirkan film yang hebat dalam mengoyak hati hingga menguras air mata, kali ini lewat How To Make Millions Before Grandma Dies atau Lahn Mah.",
+    link: "https://www.cnnindonesia.com/hiburan/20240517150039-220-1098975/review-film-how-to-make-millions-before-grandma-dies",
   },
   {
     id: 3,
     image:
-      "https://res.cloudinary.com/df82hir2r/image/upload/v1684289104/xdradio/article/weird-genius-1_169_vdid86.jpg",
-    title: "Weird Genius Rilis Lagu Glorious yang Bakar Semangat Pemuda",
-    slug: "Jakarta, CNN Indonesia -- Weird Genius resmi merilis lagu Glorious yang menjadi soundtrack Piala Dunia U-20 di Argentina. Lagu dan music video-nya tayang bersamaan tayang pada Jumat (12/5) lewat kanal YouTube FIFA.",
-    link: "https://www.cnnindonesia.com/hiburan/20230512194614-227-948970/weird-genius-rilis-lagu-glorious-yang-bakar-semangat-pemuda",
+      "https://res.cloudinary.com/df82hir2r/image/upload/v1716126715/xdradio/article/articel3_syx3ue.jpg",
+    title: "Apa Pentingnya World Water Forum 2024 di Bali?",
+    slug: "World Water Forum (WWF) ke-10 akan diselenggarakan di Bali pada 18-25 Mei 2024. Apa yang membuat gelaran internasional ini penting?",
+    link: "https://www.cnnindonesia.com/teknologi/20240517093625-199-1098768/apa-pentingnya-world-water-forum-2024-di-bali",
   },
 ];
